9a89c28c35433168d693f5fb8b880cc66c257d74
ArchanaIndrabanu <archanaindrabanu_bai26@mepcoeng.ac.in>
2025-03-22 20:20:54 +0530

    Added studentsystem folder

---
+++ src/main/java/com/arjuncodes/studentsystem/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.arjuncodes.studentsystem.controller;
+
+import com.arjuncodes.studentsystem.model.User;
+import com.arjuncodes.studentsystem.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+@CrossOrigin
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/adduser")
+    public String adduser(@RequestBody User user){
+        userService.saveUser(user);
+        return "New user is added";
+    }
+
+    @PostMapping("/checkuser")
+    public ResponseEntity<String> checkUser(@RequestBody User user) {
+        User existingUser = userService.findByUsername(user.getUsername());
+        if (existingUser != null && existingUser.getPassword().equals(user.getPassword())) {
+            return ResponseEntity.ok("Login Success");
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Incorrect Username/Password");
+        }
+}
+}
